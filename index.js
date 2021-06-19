a187215953d4e5382a384147ab908c294620a778
Harun Bajric <54305831+bajricharun@users.noreply.github.com>
2021-06-19 09:54:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 var express = require('express');
-var cors = require('cors');
 const jwt = require("jsonwebtoken");
 const path = require("path")
 
 var bodyParser = require("body-parser");
 var app = express();
-var port = process.env.PORT || 8000
+var port = process.env.PORT || 5000
 process.env.SECRET_KEY = '1280f07cd273b0c2c6d8c072308e9e91af6407bfbfb518f7a598e3a6c7f581fb'
 
 app.use(bodyParser.json())
-app.use(cors())
 app.use(
     bodyParser.urlencoded({
         extended:false
@@ -26,7 +24,7 @@ app.use('/comments', Comments);
 app.use('/likes', Likes);
 app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "src", "index.html"));
 });
 
 
