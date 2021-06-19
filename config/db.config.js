0697c9c87b1220a203c26f52b097ede5b37c00e8
Harun Bajric <54305831+bajricharun@users.noreply.github.com>
2021-06-19 14:19:31 +0200

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "harun123",
-    DB: "test"
+    HOST: "d3y0lbg7abxmbuoi.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+    USER: "jql0urnhcrgiphtz",
+    PASSWORD: "twmh9uslfjcyi33d",
+    DB: "cdt9hm112cnka842"
 };
